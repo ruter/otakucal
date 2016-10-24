7fdc65debbdd75164a2f93602aae7f11631fb1d3
Ruter <ruter.lu@gmail.com>
2016-10-24 12:54:28 +0800

    Add forms

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('remember_me', default=False)
+
+
+class RegisterForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
