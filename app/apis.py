19339f74b94742f6132371706f18d3e731e971ee
Ruter <ruter.lu@gmail.com>
2016-11-13 01:35:15 +0800

    Add query conditions

--- app/apis.py
+++ app/apis.py
@@ -9,29 +9,31 @@ import random
 from datetime import datetime
 from itertools import chain
 
-HBENTRY_MAX = HBEntry.query.count()
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(json.dumps({'error': 'Not found'}), 404)
 
-def rand_result(max_num, num):
+def rand_result(rand_num, num):
 	random.seed(datetime.utcnow())
-	rs = random.sample(range(1, max_num), num)
+	rs = random.sample(range(1, rand_num + 1), num)
 	return rs
+
+def get_entries(rand_num, num, max_num):
+	entry_id_list = rand_result(rand_num, num)
+	good_id = entry_id_list[:max_num]
+	bad_id = entry_id_list[max_num:]
+	entries = {}
+	good_list = Entry.query.with_entities(Entry.good).filter(Entry.id.in_(good_id)).all()
+	entries['good'] = [good.good for good in good_list]
+	entries['bad'] = Entry.query.with_entities(Entry.bad).filter_by(id=bad_id).first().bad
+	return entries
 
 
 class Entries(Resource):
 	def get(self):
-		entry_id_list = rand_result(Entry.query.count() + 1, 3)
-		good_id = entry_id_list[:2]
-		bad_id = entry_id_list[-1]
-		entries = {}
-		good_list = Entry.query.with_entities(Entry.good).filter(Entry.id.in_(good_id)).all()
-		entries['good'] = [good.good for good in good_list]
-		entries['bad'] = Entry.query.with_entities(Entry.bad).filter_by(id=bad_id).first().bad
 		return make_response(
-			json.dumps({'result': entries}, ensure_ascii=False).decode('utf-8'))
+			json.dumps({'result': get_entries(Entry.query.count(), 3, 2)}, ensure_ascii=False).decode('utf-8'))
 
 
 class Hobbies(Resource):
@@ -45,18 +47,47 @@ class HBEntry(Resource):
 	def get(self):
 		parser = reqparse.RequestParser()
 		parser.add_argument('hobby', action='append')
-		parser.add_argument('lucky_val', type=bool)
+		parser.add_argument('lucky', type=bool)
 		args = parser.parse_args()
+
 		hobbies = args['hobby']
+		lucky = args['lucky']
+
 		hbentries = {}
 		good = []
 		bad = []
-		for hb in hobbies:
-			hobby = Hobby.query.filter_by(hobby=hb).first()
-			good.extend([h.good for h in hobby.hb_entries])
-			bad.extend([h.bad for h in hobby.hb_entries])
-		hbentries['hobby_entries'] = {'good': good, 'bad': bad}
 
+		if hobbies:
+			entry_list = []
+			for hb in hobbies:
+				hobby = Hobby.query.filter_by(hobby=hb).first()
+				entry_list.extend([h for h in hobby.hb_entries])
+			
+			if len(entry_list) > 9:
+				entry_list = random.sample(entry_list, 9)
+				good = [e.good for e in entry_list[:6]]
+				bad = [e.bad for e in entry_list[6:]]
+				rs = get_entries(Entry.query.count(), 3, 2)
+				good.extend(rs['good'])
+				bad.extend(rs['bad'])
+			else:
+				e_num = 12 - len(entry_list)
+				entry_list = random.sample(entry_list, len(entry_list))
+				good = [e.good for e in entry_list[:len(entry_list) / 2]]
+				bad = [e.bad for e in entry_list[len(entry_list) / 2:]]
+				rs = get_entries(Entry.query.count(), e_num, e_num / 2)
+				good.extend(rs['good'])
+				bad.extend(rs['bad'])
+		else:
+			rs = get_entries(Entry.query.count(), 12, 8)
+			good.extend(rs['good'])
+			bad.extend(rs['bad'])
+
+		if lucky:
+			random.seed(datetime.utcnow())
+			hbentries['lucky_val'] = str(random.randint(60, 120)) + '%'
+
+		hbentries['hobby_entries'] = {'good': good, 'bad': bad}
 		return make_response(json.dumps({'result': hbentries}, ensure_ascii=False).decode('utf-8'))
 		
 
